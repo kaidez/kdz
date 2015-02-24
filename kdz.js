52ffbe9e4cd62112b8aca6402e02dd0c01b34e64
kaidez <kai.gittens@gmail.com>
2015-02-24 10:21:08 -0500

    update kdz.js (see message) The "GetFile" constructor function eliminates the need for lots of download-related methods already in existence...getBower(), getPackage(), etc.  So remove those methods.  Add an "else" call to the "goToTest()" function. Completely remove the old "init" command because the "app" command completely replaces it. Fix lots of whitespace.

--- kdz.js
+++ kdz.js
@@ -23,6 +23,8 @@ function goToTest() {
   if(program.test) {
     cd("init-test");
     deferred.resolve();
+  } else {
+    deferred.resolve();
   }
   return deferred.promise;
 } //end "goToTest()"
@@ -100,10 +102,6 @@ function GetFile( file, target ) {
 } //end "getFile()"
 
 
-
-
-
-
 // Helper function for creating CSS preprocessors files
 // "opt" will be a preprocessor file type: either "less" or "scss"
 function preProcess( opt ) {
@@ -138,231 +136,6 @@ function buildCoreCssPreprocess( opt ) {
 }
 
 
-
-
-
-
-
-// Helper function for downloading my core "package.json" file
-function getPackage() {
-  var download = new Download( { strip: 1 } )
-  .get('https://raw.githubusercontent.com/kaidez/kdz/master/download_source/package.json')
-  .dest('.')
-  .use(progress());
-
-  download.run(function (err) {
-    if (err) {
-      throw err;
-    }
-  });
-}
-
-
-// Helper function for downloading my core "package.json" file
-function getGrunt() {
-  var download = new Download( { strip: 1 } )
-  .get('https://raw.githubusercontent.com/kaidez/kdz/master/download_source/Gruntfile.js')
-  .dest('.')
-  .use(progress());
-
-  download.run(function (err) {
-    if (err) {
-      throw err;
-    }
-  });
-}
-// Helper function for downloading my core "gulpfile.js" file
-function getGulp() {
-  var download = new Download( { strip: 1 } )
-  .get('https://raw.githubusercontent.com/kaidez/kdz/master/download_source/gulpfile.js')
-  .dest('.')
-  .use(progress());
-
-  download.run(function (err) {
-    if (err) {
-      throw err;
-    }
-  });
-}
-
-// Helper function for downloading my core "gulpfile.js" file
-function getBowerrc() {
-  var download = new Download( { strip: 1 } )
-  .get('https://raw.githubusercontent.com/kaidez/kdz/master/download_source/.bowerrc')
-  .dest('.')
-  .use(progress());
-
-  download.run(function (err) {
-    if (err) {
-      throw err;
-    }
-  });
-}
-
-
-
-
-// Helper function for downloading my core "bower.json" file
-function getBower() {
-  var download = new Download( { strip: 1 } )
-  .get('https://raw.githubusercontent.com/kaidez/kdz/master/download_source/bower.json')
-  .dest('.')
-  .use(progress());
-
-  download.run(function (err) {
-    if (err) {
-      throw err;
-    }
-  });
-}
-
-
-// Helper function for downloading core "bootstrap.css" file
-function getGitignore() {
-  var download = new Download( { strip: 1 } )
-  .get('https://raw.githubusercontent.com/kaidez/kdz/master/download_source/.gitignore')
-  .dest('.')
-  .use(progress());
-
-  download.run(function (err) {
-    if (err) {
-      throw err;
-    }
-  });
-}
-
-
-program
-.command('init')
-.description('scaffold the project')
-.action(function(){
-  goToTest()
-  .then(function(){
-    buildFolders();
-    return Q.delay(2000);
-  })
-  .then(function(){
-    if(program.build) {
-      buildDir();
-      return Q.delay(2000);
-    }
-  }, function(){ console.log("✘ The \"build\" folder didn't build!");})
-  .then(function(){
-    if (program.gitignore) {
-      fs.open('.gitignore', "rs", function(err, fd) {
-        if (err && err.code == 'ENOENT') {
-          // If ".gitignore" does NOT exist, download it
-          console.log(chalk.green.underline("Download .gitignore...\n"));
-          getGitignore();
-        } else {
-          console.log( chalk.red('".gitignore" exists...don\'t download it.\n' ) );
-          fs.close(fd);
-        }
-      });
-    }
-    return Q.delay(2000);
-  }, function(){ console.log("✘ .gitignore file failed to download!");})
-  .then(function(){
-    buildCoffee();
-  }, function(){ console.log("✘ main.coffee build failed!");})
-  .then(function(){
-    if(program.less) {
-      console.log( chalk.green.underline( "Building .less preprocessor files...\n" ) );
-      preProcess("less");
-    } else if(program.scss){
-      console.log( chalk.green.underline( "Building .scss preprocessor files...\n" ) );
-      preProcess("sass");
-    }
-    return Q.delay(2000);
-  }, function(){ console.log("✘ CSS preprocess files failed to build!");})
-  .then(function(){
-    if(program.less) {
-      console.log(chalk.green.underline("Download style.less...\n"));
-      buildCoreCssPreprocess("less");
-    } else if (program.scss) {
-      console.log(chalk.green.underline("Download style.scss...\n"));
-      buildCoreCssPreprocess("scss");
-    }
-    return Q.delay(2000);
-  }, function(){ console.log("✘ Core preprocess file failed to download!");})
-  .then(function(){
-    fs.open('gulpfile.js', "rs", function(err, fd) {
-      if (err && err.code == 'ENOENT') {
-        // If "gulpfile.js" does NOT exist, download it
-        console.log(chalk.green.underline("Download gulpfile.js...\n"));
-        getGulp();
-      } else {
-        console.log( chalk.red('"gulpfile.js" exists...don\'t download it.\n' ) );
-        fs.close(fd);
-      }
-    });
-    return Q.delay(2000);
-  }, function(){ console.log("✘ gulpfile.js failed to download!");})
-  .then(function(){
-    fs.open('.bowerrc', "rs", function(err, fd) {
-      if (err && err.code == 'ENOENT') {
-        // If ".bowerrc" does NOT exist, download it
-        console.log(chalk.green.underline("Download .bowerrc...\n"));
-        getBowerrc();
-      } else {
-        console.log( chalk.red('".bowerrc" exists...don\'t download it.\n' ) );
-        fs.close(fd);
-      }
-    });
-    return Q.delay(2000);
-  }, function(){ console.log("✘ .bowerrc failed to download!");})
-  .then(function(){
-    fs.open('Gruntfile.js', "rs", function(err, fd) {
-      if (err && err.code == 'ENOENT') {
-        // If "Gruntfile.js" does NOT exist, download it
-        console.log(chalk.green.underline("Download Gruntfile.js...\n"))
-        getGrunt();
-      } else {
-        console.log( chalk.red('"Gruntfile" exists...don\'t download it.\n' ) );
-        fs.close(fd);
-      }
-    });
-    return Q.delay(2000);
-  }, function(){ console.log("✘ Gruntfile.js failed to download!");})
-  .then(function(){
-    fs.open('package.json', "rs", function(err, fd) {
-      if (err && err.code == 'ENOENT') {
-        // If "package.json" does NOT exist, download it
-        console.log(chalk.green.underline("Download package.json..\n"));
-        getPackage();
-      } else {
-        console.log( chalk.red('"package.json" exists...don\'t download it.\n' ) );
-        fs.close(fd);
-      }
-    });
-    return Q.delay(2000);
-  }, function(){ console.log("✘ package.json failed to download!");})
-  .then(function(){
-    fs.open('bower.json', "rs", function(err, fd) {
-      if (err && err.code == 'ENOENT') {
-        // If "bower.json" does not exist, download it
-        console.log(chalk.green.underline("Download bower.json...\n"));
-        getBower();
-      } else {
-        console.log( chalk.red('"bower.json" exists...don\'t download it.\n' ) );
-        fs.close(fd);
-      }
-    });
-    return Q.delay(2000);
-  }, function(){console.log( chalk.red.bold( "✘ bower.json failed to download!") );})
-  .done(function(){
-    console.log( chalk.yellow.bold.underline( "THE PROJECT IS SCAFFOLDED!!") );
-    console.log( chalk.yellow( "Next steps...\n") );
-    console.log( chalk.yellow( "1. fill in the following fields in \"package.json\"") );
-    console.log( chalk.yellow( "   -name, version, homepage, description, main and git URL\n") );
-    console.log( chalk.yellow( "2. fill in the following fields in \"bower.json\"") );
-    console.log( chalk.yellow( "   -name, version, homepage, description and main\n") );
-    console.log( chalk.yellow( "3. Run \"npm-check-updates\" to check for project modules updates") );
-    console.log( chalk.yellow( "4. Run \"bower list\" to check for front-end dependency updates") );
-    console.log( chalk.yellow( "5. Run \"npm install\" and \"bower install\"") );
-
-  });
-});
 
 // "app" command: scaffolds out a SPA-like project
 program
@@ -409,26 +182,26 @@ program
     var gulp = new GetFile("gulpfile.js", ".");
     return Q.delay(2000);
   }, function(){console.log( chalk.red.bold( "✘ gulpfile.js failed to download!") );})
-    .then(function(){
-      if(program.less) {
-        console.log( chalk.green.underline( "Building .less preprocessor files...\n" ) );
-        preProcess("less");
-      } else if(program.scss){
-        console.log( chalk.green.underline( "Building .scss preprocessor files...\n" ) );
-        preProcess("sass");
-      }
-      return Q.delay(2000);
-    }, function(){ console.log("✘ CSS preprocess files failed to build!");})
-    .then(function(){
-      if(program.less) {
-        console.log(chalk.green.underline("Download style.less...\n"));
-        buildCoreCssPreprocess("less");
-      } else if (program.scss) {
-        console.log(chalk.green.underline("Download style.scss...\n"));
-        buildCoreCssPreprocess("scss");
-      }
-      return Q.delay(2000);
-    }, function(){ console.log("✘ Core preprocess file failed to download!");})
+  .then(function(){
+    if(program.less) {
+      console.log( chalk.green.underline( "Building .less preprocessor files...\n" ) );
+      preProcess("less");
+    } else if(program.scss){
+      console.log( chalk.green.underline( "Building .scss preprocessor files...\n" ) );
+      preProcess("sass");
+    }
+    return Q.delay(2000);
+  }, function(){ console.log("✘ CSS preprocess files failed to build!");})
+  .then(function(){
+    if(program.less) {
+      console.log(chalk.green.underline("Download style.less...\n"));
+      buildCoreCssPreprocess("less");
+    } else if (program.scss) {
+      console.log(chalk.green.underline("Download style.scss...\n"));
+      buildCoreCssPreprocess("scss");
+    }
+    return Q.delay(2000);
+  }, function(){ console.log("✘ Core preprocess file failed to download!");})
   .done(function(){
     console.log( chalk.yellow.bold.underline( "THE PROJECT IS SCAFFOLDED!!") );
     console.log( chalk.yellow( "Next steps...\n") );
