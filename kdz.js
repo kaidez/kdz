c6cde0f401fa6db89e3062540d72c6a2818b9151
kaidez <kai.gittens@gmail.com>
2015-02-25 12:55:02 -0500

    auto-indent kdz.js

--- kdz.js
+++ kdz.js
@@ -183,7 +183,7 @@ program
     buildFolders();
     return Q.delay( 2000 );
   })
-    .then(buildCoffee, function() { console.log( '✘ main.coffee build failed!' );})
+  .then(buildCoffee, function() { console.log( '✘ main.coffee build failed!' );})
   .then(function() {
     if( program.build ) {
       buildDir();
