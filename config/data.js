7593809132be364e795e36cd0d1871f5b3cf6021
kaidez <kai.gittens@gmail.com>
2015-03-05 20:09:08 -0500

    update file data

--- config/data.js
+++ config/data.js
@@ -1,28 +1,19 @@
 var data = {
-  "preprocess_files": [
-    "variables",
-    "mixins",
-    "bootstrap-override",
-    "retina-media-queries",
-    "globals",
-    "mobile_first",
-    "min-width-768",
-    "min-width-992",
-    "min-width-992-max-width-1199",
-    "min-width-1200",
-    "screen-and-max-width-767",
-    "screen-webkit-min-device-pixel-ratio-0",
-    "max-device-width-480-orientation-landscape",
-    "max-width-767",
-    "screen-and-min-width-768",
-    "all-transform-3d-webkit-transform-3d",
-    "min-width-768-max-width-991"
+  "core": [
+    ".bowerrc",
+    "bower",
+    "Gruntfile.js",
+    "gulpfile.js",
+    "package.json"
   ],
-  "download_files": {
-    "bower": "https://raw.githubusercontent.com/kaidez/kdz/master/bower.json",
-    "package": "https://raw.githubusercontent.com/kaidez/kdz/master/package.json",
-    "gitignore": "https://raw.githubusercontent.com/kaidez/kdz/master/.gitignore"
-    }
+  "less": [
+    "less.zip",
+    "style.less"
+  ],
+  "sass": [
+    "sass.zip",
+    "style.scss"
+  ]
 }
 
 module.exports = data;
