65c96b98f0e9dbd96b3336d7bb501ba521c42bca
kaidez <kai.gittens@gmail.com>
2015-03-09 09:16:21 -0400

    update "doneMessage()

--- kdz.js
+++ kdz.js
@@ -323,11 +323,11 @@ function unzip() {
 // Output a console message after "app" is done
 function doneMessage() {
   console.log( chalk.yellow.bold.underline( 'THE PROJECT IS SCAFFOLDED!!') );
-  console.log( chalk.yellow.bold.underline('============================\n') );
   console.log( chalk.yellow( 'Next steps...\n') );
   console.log( chalk.yellow( 'You will need to fill in fields in \"package.json\" and \"bower.json\"\n') );
-  console.log( chalk.yellow( '3. You will need to run \"npm-check-updates\" to check for project modules updates and \"bower list\" to check for front-end dependency updates') );
-  console.log( chalk.yellow( 'Run \"npm install\" and \"bower install\" after that') );
+  console.log( chalk.yellow( 'Run \"npm-check-updates\" to check for project modules updates\n') );
+  console.log( chalk.yellow( 'Run \"bower list\" to check for front-end dependency updates\n') );
+  console.log( chalk.yellow( 'Run \"npm install\" and \"bower install\" after that\n') );
 } // end "doneMessage()"
 
 
